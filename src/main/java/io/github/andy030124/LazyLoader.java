492cafb882f2b6a329f9d135ce37c3b19dff5b9e
Kirlen <garciandy579@gmail.com>
2024-06-06 12:38:59 -0400

    Updated Loader and implemented Lazy Loader

--- src/main/java/io/github/andy030124/LazyLoader.java
+++ src/main/java/io/github/andy030124/LazyLoader.java
@@ -4,29 +4,27 @@ import javafx.stage.Stage;
 /* IMPORTS ALL */
 import java.lang.reflect.*;
 
-/**
- * Principal Interface Implementation for use JRouterFX
- * @author Andy Garcia
- * @version 0.1, 2024/5/6
-*/
-public class Loader extends MainLoader {
-    
+public class LazyLoader extends MainLoader {
+
     /**
      * Constructor for initialize Object and load the first folders parse
      * @param projectName the project name for load first place
      * @param routesFolder the routes folder for load first place
      * @param mainStage the main stage of the FXML application
      */
-    public Loader(String projectName, String routesFolder, Stage mainStage){
+    public LazyLoader(String projectName, String routesFolder, Stage mainStage){
         super(projectName, routesFolder, mainStage);
         load();
     }
-    
+
+
+    /* INTERFACE METHODS */
+
     /**
-     * Load in specific project folder, and load routes controllers
+     * Load & launch only the main page
      */
     @Override
-    public void load(){
+    public void load() {
         String projectName = _projectName; 
         String routesFolder = _routesName;
 
@@ -35,74 +33,37 @@ public class Loader extends MainLoader {
         String strt = endSanitizer(_projectFolder);
         routesFolder = endSanitizer(routesFolder);
         String projectNameFolder = endSanitizer(projectName);
-        
+
         for(String file: FolderParser.printDir(
             strt + projectNameFolder
             ,strt + projectNameFolder + routesFolder,".class"
         )){
-
             String pkgName = pkg.concat(
                 "." + file.replaceAll("/", ".")
             );
 
-            try {
-                
-                Class<?> cls = getClass().getClassLoader().loadClass(pkgName);
-                
-                if(
+            if(
                 (
-                    file.toLowerCase().contains("index") 
-                    || file.toLowerCase().contains("_index")
+                    !file.toLowerCase().equals("index") 
+                &&  !file.toLowerCase().equals("_index")
+                &&  !file.toLowerCase().equals("_index_")
                 )
-                && !file.contains("/") ){
-                    file = "/";
-                }else{
-                    String[] parts = file.split("/");
-                    if( 
-                        file.contains("/") 
-                        && (parts[parts.length-1])
-                        .toLowerCase().contains("index")
-                    ){
+                ||  file.contains("/")
+            ){ continue; }
 
-                        String reconstruct = parts[0];
-                        for(int i=1; i < (parts.length-1); i++)
-                            reconstruct = endSanitizer(reconstruct) + parts[i];
-
-                        _routes.put(reconstruct, cls);
-                        System.out.println(
-                            "Loaded " 
-                            + pkgName + 
-                            " route: " 
-                            + reconstruct
-                        );                
-                    }
-                }
-
+            try {
+                Class<?> cls = getClass().getClassLoader().loadClass(pkgName);
+                file = "/";
                 _routes.put(file, cls);
                 System.out.println("Loaded " + pkgName + " route: " + file);
-
+                break;
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
-
-        } // for
-
-        // finding default page
-        for(String Key: _routes.keySet()){
-            if(
-                ((
-                    Key.toLowerCase().contains("index") 
-                    || Key.toLowerCase().contains("_index")
-                )
-                && !Key.contains("/") )
-                || (Key.hashCode() == "/".hashCode())
-            ){
-                    launch(Key);
-                    break;
-            }
         }
 
-    } // method load
+        if( _routes.containsKey("/") ) launch("/");
+    }
 
     /**
      * Use route to execute the constructor controller
@@ -110,10 +71,12 @@ public class Loader extends MainLoader {
      * @return Object of instance Controller route (maybe null)
      */
     @Override
-    public Object launch(String route){
+    public Object launch(String route) {
         Object ret = null;
 
         Class<?> cls = _routes.get(route);
+        if( cls == null )
+            cls = find(route);
         String errSms = "Error while launch route -> " + route + " [";
         try {
 
@@ -137,5 +100,5 @@ public class Loader extends MainLoader {
         );
         return ret; 
     }
-
+    
 }
