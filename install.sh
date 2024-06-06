e53dbc5ac97cdf43327e3d4778fe8cdf92c27482
Kirlen <garciandy579@gmail.com>
2024-06-06 17:51:12 -0400

    prepare configurations for compilations and installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bash ./release.sh && bash ./docs.sh && mvn install:install-file \
+-Dfile=./target/jrouterfx-0.0.1.jar \
+-DpomFile=./pom.xml \
+-Dsources=./target/jrouterfx-0.0.1-sources.jar \
+-Djavadoc=./target/jrouterfx-0.0.1-javadoc.jar \
+-DgroupId=io.github.andy030124 \
+-DartifactId=jrouterfx \
+-Dversion=0.0.1 \
+-Dpackaging=jar \
+-DcreateChecksum=true
+
