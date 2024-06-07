2b9fefe66a6af152867dca3ecf1348fdb72c1c5d
Kirlen <garciandy579@gmail.com>
2024-06-06 20:51:09 -0400

    Addeds Unuseds Constructors for JavaDoc Documentation

--- src/main/java/io/github/andy030124/JRouterFX.java
+++ src/main/java/io/github/andy030124/JRouterFX.java
@@ -3,24 +3,51 @@ import io.github.andy030124.hooks.*;
 
 /**
  * Global Class for access to Hooks and all Apis
- */
+*/
 public class JRouterFX {
+    /**
+     * !Unused
+     */
+    public JRouterFX(){}
+
     /**
      * Class for wrapper of hooks
      */
-    public class Hooks{
+    public static class Hooks{
+        /**
+        * !Unused
+        */
+        public Hooks(){}
+
         /**
          * Class with the useState apis
          */
-        public class State extends StatesHook{}
+        public static class State extends StatesHook{
+            /**
+            * !Unused
+            */
+            public State(){}
+        }
         /**
          * Class with the useSignal apis
          */
-        public class Signal extends SignalHook{}
+        public static class Signal extends SignalHook{
+            /**
+             * !Unused
+            */
+            public Signal(){}
+        }
     }
 
     /**
      * Class for wrapper of hooks
      */
-    public class Loaders extends LoadersHook {}
+    public static class Loaders extends LoadersHook {
+        /**
+        * !Unused
+        */
+        public Loaders(){}
+    }
+
+    
 }
