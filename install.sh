2173f16b98f64d6bc5fc8830e146dec4c6e9d61b
Kirlen <garciandy579@gmail.com>
2024-06-30 14:14:20 -0400

    Updated the install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mvn clean && bash ./release.sh && mvn install:install-file \
--Dfile=./target/jrouterfx-0.0.2.jar \
+-Dfile=./target/jrouterfx-0.1.0.jar \
 -DpomFile=./pom.xml \
 -DgroupId=jrouterfx \
 -DartifactId=jrouterfx \
--Dversion=0.0.2 \
+-Dversion=0.1.0 \
 -Dpackaging=jar \
 -DcreateChecksum=true
